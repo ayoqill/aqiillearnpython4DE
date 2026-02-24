b095e5bbcf752ec8797948a7d7806373126d1278
aqil <aqilaiman058@gmail.com>
2026-02-24 13:20:24 +0800

    basic fundamental

---
+++ lvl1-D1.py
@@ -0,0 +1,19 @@
+"""
+print("Hi there! Welcome to Level 1 of the education. Let's get started!")
+print('Hello Aqil')
+
+Use triple quotes to write a multi-line string comments. This is useful for writing long messages.
+Use # to write comments.
+"""
+#print ("Hello Aqil", 260) - can combine string and number in print statement, but it will convert number to string.
+
+# Python is interpreter language, it executes code line by line.
+# Python is dynamically typed language, we don't need to declare variable type.
+
+# Being Data Engineer must always use python environment, because python has many libraries for data engineering such as pandas, numpy, etc.
+
+# Delimiters (sep) - use to separete code into blocks
+
+#print("Hello Aqil", 260, "is your username", sep='*')
+
+#print('Hello \'AQIL AIMAN\'. This is a test for pyspark, as we need to master the string manipulation in python for data engineering.')
