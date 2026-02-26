dc34b8eefa9675edde8cef69558b2a6ffff68b7c
aqil <aqilaiman058@gmail.com>
2026-02-26 23:02:12 +0800

    day 2

---
+++ lvl1-D2-DS.py
@@ -0,0 +1,21 @@
+# Data Structures in .py
+# means efficient way to store and organize data in Python. 
+
+# A list
+"""
+my_list = [1, 2, 'aqil', [0,1,2], True]
+
+print(my_list[3][1]) # Accessing the second element of the nested list [0,1,2] which is 1.
+
+"""
+
+"""
+my_list.append("Ikan")
+
+print(my_list)
+"""
+
+# list are mutabale, which means can be edited.
+
+# no need to like my_list = my_list.append("Ikan")
+
