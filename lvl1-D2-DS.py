bb214e5c80e1ebc1eb858868e079c2ec94d5edad
aqil <aqilaiman058@gmail.com>
2026-02-28 14:36:53 +0800

    data structure settle

--- lvl1-D2-DS.py
+++ lvl1-D2-DS.py
@@ -19,3 +19,30 @@ print(my_list)
 
 # no need to like my_list = my_list.append("Ikan")
 
+
+#A Dictionary
+
+"""
+my_dict = {
+            "name": "aqil",
+            "age": 20,
+            "hobbies": ["coding", "gaming", "traveling"],
+    }
+
+print(my_dict["hobbies"][0]) 
+print(my_dict)
+print(my_dict.keys())
+print(my_dict.items())
+
+"""
+
+# a Tuple
+
+my_tuple = (1, 2, 'aqil', True)
+
+my_tuplist = list(my_tuple) # Convert the tuple to a list to make it mutable.
+
+my_tuplist.append("Ikan") # Now we can append to the list.
+
+my_tuple = tuple(my_tuplist) # Convert it back to a tuple.
+
