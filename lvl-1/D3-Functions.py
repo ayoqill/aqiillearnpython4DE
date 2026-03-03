d5727199baca7d4827d981830fcc77c0c368e46b
aqil <aqilaiman058@gmail.com>
2026-03-03 16:13:58 +0800

    func

--- lvl-1/D3-Functions.py
+++ lvl-1/D3-Functions.py
@@ -1,5 +1,6 @@
 # Use function to make the code reuseable and organize. jimat ruang code
 
+"""
 def my_function(x):
 
     if (x > 10):
@@ -12,3 +13,35 @@ def my_function(x):
 
 returned_val = my_function(20)
 print(returned_val)
+"""
+
+# Function with multiple parameters
+
+""""
+def greet(name, age):
+
+    return(name, age)
+
+greeting = greet("aqil", 22)
+
+print(greeting)
+"""
+
+# What if the input/data is so many? make it into tuple
+
+"""
+def biodata(*bio):
+
+    return bio
+
+my_bio = biodata("aqil", 22, "coding", "gaming", "traveling")
+print(my_bio)
+"""
+
+# Function with keyword arguments, change it to dictionary
+def biodata(**bio):
+
+    return bio
+
+my_bio = biodata(name="aqil", age=22, hobby="coding", hobby2="gaming", hobby3="traveling")
+print(my_bio)
