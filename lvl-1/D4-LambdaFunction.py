7be3f781765446ddd6e0cf0ae11348e6c0a4fab9
aqil <aqilaiman058@gmail.com>
2026-03-03 17:00:41 +0800

    lambda func

---
+++ lvl-1/D4-LambdaFunction.py
@@ -0,0 +1,8 @@
+# Lambda functions are anonymous functions that can be defined in a single line.
+# They are often used for short, simple functions that are not reused elsewhere in the code.
+# the function is not stored in a variable, but it can be assigned to a variable for later use. It just print
+
+# Example of a lambda function that adds two numbers
+add = lambda x, y: x + y
+result = add(99,70)
+print(result)
