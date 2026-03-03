f314dc26d33097ca0dc5ef2d2b712e031b042b8c
aqil <aqilaiman058@gmail.com>
2026-03-03 18:38:45 +0800

    map

---
+++ lvl-1/D4-map,filter,reduce.py
@@ -0,0 +1,9 @@
+# can handle individual value and perform operation on each of them in a list or any iterable.
+
+my_list = [1, 2, 3, 4, 5]
+
+def square(item):
+    return item*item
+
+result = my_list(map(square, my_list)) # map(function, iterable)
+print(result)
