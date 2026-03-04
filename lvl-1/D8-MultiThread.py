33ad0f56bcfd6048c6c289dd6eb2779ed04da2b7
aqil <aqilaiman058@gmail.com>
2026-03-04 22:34:00 +0800

    Multi Thread

---
+++ lvl-1/D8-MultiThread.py
@@ -0,0 +1,20 @@
+# Multi Threading is a technique that allows a program to run multiple threads of execution concurrently.
+# This can be useful for improving the performance of a program by allowing it to perform multiple tasks at the same time.
+# make it run faster and handy for Data Engineer, because we can run multiple tasks at the same time, which can save time and increase efficiency.
+
+import time
+import random
+from concurrent.futures import ThreadPoolExecutor
+
+tables = ["table1", "table2", "table3", "table4", "table5"]
+
+
+def task(i):
+
+    wait_time = random.randint(1, 3) 
+    print(f"Task {i} is running, will take {wait_time} seconds to complete.")
+    time.sleep(wait_time)
+    print(f"Task {i} is completed.")
+
+for i in tables:
+    task(i)
